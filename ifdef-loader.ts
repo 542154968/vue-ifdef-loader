40405e4e7090a00b7d757f91f385dcc6d56aafc7
542154968 <542154968@qq.com>
2022-05-23 14:49:48 +0800

    feat(): 扩展匹配规则支持<!--和-->，插件参数替换占位符默认为true，默认值为空
    原理就是替换掉这个标志为空

--- ifdef-loader.ts
+++ ifdef-loader.ts
@@ -1,49 +1,57 @@
-import * as loaderUtils from 'loader-utils';
-import { parse } from './preprocessor';
-import * as path from 'path';
+import * as loaderUtils from "loader-utils";
+import { parse } from "./preprocessor";
+import * as path from "path";
 
-export = function(source: string, map) {
-   this.cacheable && this.cacheable();
+export = function (source: string, map) {
+  this.cacheable && this.cacheable();
 
-   const options: loaderUtils.OptionObject = loaderUtils.getOptions(this) || {};
-   const originalData = options.json || options;
+  const options: loaderUtils.OptionObject = loaderUtils.getOptions(this) || {};
+  const originalData = options.json || options;
 
-   const data = { ...originalData };
+  const data = { ...originalData };
 
-   const verboseFlag = "ifdef-verbose";
-   const verbose = data[verboseFlag];
-   if(verbose !== undefined) {
-      delete data[verboseFlag];
-   }
+  const verboseFlag = "ifdef-verbose";
+  const verbose = data[verboseFlag];
+  if (verbose !== undefined) {
+    delete data[verboseFlag];
+  }
 
-   let filePath: string | undefined = undefined;
-   if(verbose) {
-      filePath = path.relative(this.rootContext || '', this.resourcePath);
-   }
+  let filePath: string | undefined = undefined;
+  if (verbose) {
+    filePath = path.relative(this.rootContext || "", this.resourcePath);
+  }
 
-   const tripleSlashFlag = "ifdef-triple-slash";
-   const tripleSlash = data[tripleSlashFlag];
-   if(tripleSlash !== undefined) {
-      delete data[tripleSlashFlag];
-   }
+  const tripleSlashFlag = "ifdef-triple-slash";
+  const tripleSlash = data[tripleSlashFlag];
+  if (tripleSlash !== undefined) {
+    delete data[tripleSlashFlag];
+  }
 
-   const fillWithBlanksFlag = "ifdef-fill-with-blanks";
-   const fillWithBlanks = data[fillWithBlanksFlag];
-   if(fillWithBlanks !== undefined) {
-      delete data[fillWithBlanksFlag];
-   }
+  const fillWithBlanksFlag = "ifdef-fill-with-blanks";
+  const fillWithBlanks = data[fillWithBlanksFlag];
+  if (fillWithBlanks !== undefined) {
+    delete data[fillWithBlanksFlag];
+  }
 
-   const uncommentPrefixFlag = "ifdef-uncomment-prefix";
-   const uncommentPrefix = data[uncommentPrefixFlag];
-   if(uncommentPrefix !== undefined) {
-      delete data[uncommentPrefixFlag];
-   }
+  const uncommentPrefixFlag = "ifdef-uncomment-prefix";
+  const uncommentPrefix = data[uncommentPrefixFlag];
+  if (uncommentPrefix !== undefined) {
+    delete data[uncommentPrefixFlag];
+  }
 
-   try {
-      source = parse(source, data, verbose, tripleSlash, filePath, fillWithBlanks, uncommentPrefix);
-      this.callback(null, source, map);
-   } catch(err) {
-      const errorMessage = `ifdef-loader error: ${err}`;
-      this.callback(new Error(errorMessage));
-   }
+  try {
+    source = parse(
+      source,
+      data,
+      verbose,
+      tripleSlash,
+      filePath,
+      fillWithBlanks,
+      uncommentPrefix
+    );
+    this.callback(null, source, map);
+  } catch (err) {
+    const errorMessage = `ifdef-loader error: ${err}`;
+    this.callback(new Error(errorMessage));
+  }
 };
